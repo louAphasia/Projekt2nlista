e563e9cbcab0b49dd6a31ab12cedc5be495e28ef
louAphasia <42347224+louAphasia@users.noreply.github.com>
2019-05-24 16:01:16 +0200

    dodaj z jogi

--- main.py
+++ main.py
@@ -6,38 +6,43 @@ def main():
 
     lista=MLista(capacity)
 
+
+
     while True:
-        polecenie = input("Wybierz pol (pisz, dodaj_element, znajdz, pobierz, rozmiar, pojemnosc,"
-                          " usun_powtorzenia, odwroc, zwieksz_pojemnosc, zmniejsz_pojemnosc, wyjscie): ")
+        polecenie = input("Wybierz pol (pisz, de, znajdz, pobierz, rozmiar, pojemnosc,"
+                          " usun, odwroc, zwieksz, zmniejsz, wyjscie): ")
         if polecenie == "pisz":
             lista.pisz()
 
-        elif polecenie == "dodaj_element":
+        elif polecenie == "de":
             element=input(" Podaj wartosc elementu: ")
+            lista.dodaj_element(element)
+
+
         elif polecenie == "znajdz":
             pass
         elif polecenie == "pobierz":
             pass
         elif polecenie == "rozmiar":
-            pass
+            lista.rozmiar()
         elif polecenie == "pojemnosc":
             pass
-        elif polecenie == "usun_powtorzenie":
+        elif polecenie == "usun":
             pass
         elif polecenie == "odwroc":
             pass
-        elif polecenie == "zwieksz_pojemnosc":
+        elif polecenie == "zwieksz":
             pass
-        elif polecenie == "zmniejsz pojemnosc":
+        elif polecenie == "zmniejsz":
             pass
         elif polecenie == "wyjscie":
             break
         else:
-            print("Bledna operacja ")
+            print("Bledne polecenie ")
 
 
 
-    lista.pisz()
+
 
     print(lista.rozmiar())
 
--- mlist.py
+++ mlist.py
@@ -1,21 +1,25 @@
-class MLista:
+class MLista(list):
 
     def __init__(self, capacity):
         self.capacity=capacity
-        self.elementy=capacity*[]
+        self.elementy=[]
 
     def rozmiar(self):
         size=int()
         for x in self.elementy:
-            size=+1
+            size+=1
         return size
 
     def pisz(self):
-        print("pojemność: " , self.capacity)
-        print("Rozmiar aktualny listy:", MLista.rozmiar(self),"Elementy: " , self.elementy)
+        print("Rozmiar aktualny listy:", MLista.rozmiar(self))
+        print(" Pojemnosc listy : ",self.capacity)
+        print("Elementy: " , self.elementy)
 
     def dodaj_element(self, x):
-        self.elementy.append(x)
+        if self.capacity>len(self.elementy):
+            self.elementy.append(x)
+        else:
+            print( "blad")
         return self.elementy
 
     def znajdz(self, x):
